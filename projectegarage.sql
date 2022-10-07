fb4a87000a6ae0198aea63e53641acaaf53e37c3
CharacterArc <91706093+CharacterArc@users.noreply.github.com>
2022-10-07 16:39:22 -0500

    Update projectegarage.sql

--- projectegarage.sql
+++ projectegarage.sql
@@ -1,11 +1,11 @@
 CREATE TABLE `user` (
   id bigint NOT NULL AUTO_INCREMENT,
-  fname varchar(30) DEFAULT NULL, 
-  lname varchar(30) DEFAULT NULL,
-  username varchar(20) NOT NULL,
+  fName varchar(30) DEFAULT NULL, 
+  lName varchar(30) DEFAULT NULL,
+  userName varchar(20) NOT NULL,
+  passwordHash varchar(32) NOT NULL,
   email varchar(50) DEFAULT NULL,
-  passwordhash varchar(32) NOT NULL,
-  schoolname varchar(50) DEFAULT NULL,
+  schoolName varchar(50) DEFAULT NULL,
   address varchar(50) DEFAULT NULL,
   createdBy DATETIME NOT NULL,
   PRIMARY KEY (id)
@@ -13,7 +13,7 @@ CREATE TABLE `user` (
 
 CREATE TABLE inventory (
   id bigint NOT NULL AUTO_INCREMENT,
-  sellerid bigint NOT NULL,
+  sellerId bigint NOT NULL,
   title varchar(75) NOT NULL,
   author varchar(60) NOT NULL,
   isbn bigint NOT NULL,
@@ -21,34 +21,34 @@ CREATE TABLE inventory (
   sold bool DEFAULT FALSE,
   createdBy DATETIME NOT NULL,
   PRIMARY KEY (id),
-  FOREIGN KEY (sellerid) REFERENCES `user`(id)
+  FOREIGN KEY (sellerId) REFERENCES `user`(id)
 );
 
 CREATE TABLE cart (
 id bigint NOT NULL AUTO_INCREMENT,
-buyerid bigint NOT NULL,
+buyerId bigint NOT NULL,
 totalPrice float,
 createdBy DATETIME NOT NULL,
 PRIMARY KEY (id),
-FOREIGN KEY (buyerid) REFERENCES `user`(id)
+FOREIGN KEY (buyerId) REFERENCES `user`(id)
 );
 
 CREATE TABLE cart_item (
 id bigint NOT NULL AUTO_INCREMENT,
-itemid bigint NOT NULL,
-cartid bigint NOT NULL,
+itemId bigint NOT NULL,
+cartId bigint NOT NULL,
 createdBy DATETIME NOT NULL,
 PRIMARY KEY (id),
-FOREIGN KEY (cartid) REFERENCES cart(id),
-FOREIGN KEY (itemid) REFERENCES inventory(id)
+FOREIGN KEY (cartId) REFERENCES cart(id),
+FOREIGN KEY (itemId) REFERENCES inventory(id)
 );
 
 CREATE TABLE transactions (
 id bigint NOT NULL AUTO_INCREMENT,
-buyerid bigint NOT NULL,
-cartid bigint NOT NULL,
+buyerId bigint NOT NULL,
+cartId bigint NOT NULL,
 createdBy DATETIME NOT NULL,
 PRIMARY KEY (id),
-FOREIGN KEY (buyerid) REFERENCES `user`(id),
-FOREIGN KEY (cartid) REFERENCES cart (id)
+FOREIGN KEY (buyerId) REFERENCES `user`(id),
+FOREIGN KEY (cartId) REFERENCES cart (id)
 );

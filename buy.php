ae6f48deae57b684d89140360a35ce714d9691c2
Zarya <zarya1997@gmail.com>
2022-11-21 21:12:37 -0600

    Created a buy page
    -User can search for books by title or ISBN\n-User can look for every listed book on the database by leaving the search bar empty

---
+++ buy.php
@@ -0,0 +1,51 @@
+<html>
+    <body>
+        <title>Search</title>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css">
+        <link rel="stylesheet" href="style.css">
+        <style type="text/css">
+        body {
+            background-image: url('Back.jpg');
+            background-size:cover;
+        }
+        </style>
+        <table>
+            <tr>
+                <th>Title</th>
+                <th>Author</th>
+                <th>ISBN</th>
+                <th>Price</th>
+            </tr>
+            <?php
+            session_start();
+
+            $id = $_SESSION["user_id"];
+
+            $mysqli = require __DIR__ . "/yardsaledb.php";
+            $search = mysqli_real_escape_string($mysqli, $_POST["search"] );
+                
+            if (empty($_POST["search"])) {
+                $sql = "SELECT title,author,isbn,price FROM inventory";
+            }
+            else {
+                $sql = "SELECT title,author,isbn,price FROM inventory
+                        WHERE isbn LIKE '%$search%' OR title LIKE '%$search%'";
+            }
+
+            $result = $mysqli->query($sql);
+            $check = mysqli_num_rows($result);
+            if ($check > 0) {
+                while ($row = $result->fetch_assoc()) {
+                    echo "<tr><td>" . $row["title"] . "</td><td>" . $row["author"] . "</td><td>" . $row["isbn"] . "</td><td>" . $row["price"] . "</td></tr>";
+                }
+            }
+            else {
+                header("Location: noresults.html");
+                exit;
+            }
+                
+            ?>
+        </table>
+    </body>
+</html>
